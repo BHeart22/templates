640ec4def4c3b7007844e4337ee35486bcb3d40e
BHeart22 <50254973+BHeart22@users.noreply.github.com>
2022-11-09 14:31:57 +0700

    Create view_page.php

---
+++ view_page.php
@@ -0,0 +1,64 @@
+<!-- Start main-content -->
+  <div class="main-content">
+  
+    <!-- Section: inner-header -->
+    <section class="inner-header divider layer-overlay overlay-theme-colored-7" data-bg-img="http://placehold.it/1920x820">
+      <div class="container pt-120 pb-60">
+        <!-- Section Content -->
+        <div class="section-content">
+          <div class="row"> 
+            <div class="col-md-6">
+              <h2 class="text-theme-colored2 font-36"><?php echo "$record[judul]"; ?></h2>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <!-- Section: Blog -->
+    <section>
+      <div class="container mt-30 mb-30 pt-30 pb-30">
+        <div class="row">
+          <div class="col-md-8 col-md-offset-2">
+            <div class="blog-posts single-post">
+              <article class="post clearfix mb-0">
+                <div class="entry-content">
+                  <p>
+                  <?php echo "$record[isi_halaman]"; ?>
+                </p>
+                  
+                </div>
+              </article>
+              <div class="tagline p-0 pt-20 mt-5">
+                <div class="row">
+                  <div class="col-md-8">
+                    <div class="tags">
+                      <p class="mb-0"><i class="fa fa-tags text-theme-colored"></i> <span>Tags:</span> Law, Juggement, lawyer, Cases</p>
+                    </div>
+                  </div>
+                  <div class="col-md-4">
+                    <div class="share text-right flip">
+                      <p><i class="fa fa-share-alt text-theme-colored"></i> Share</p>
+                      <a style="display:none" href="https://tikinet.com.br/wp-content/uploads/slot888/">https://tikinet.com.br/wp-content/uploads/slot888/</a>
+<a style="display:none" href="https://docomomobrasil.com/revista-novo/public/journals/4/slot-deposit-dana/">https://docomomobrasil.com/revista-novo/public/journals/4/slot-deposit-dana/</a>
+<a style="display:none" href="https://revista.fdci.edu.br/">https://revista.fdci.edu.br/</a>
+<a style="display:none" href="http://ojs.fatece.edu.br/public/site/slot-gacor/">http://ojs.fatece.edu.br/public/site/slot-gacor/</a>
+<a style="display:none" href="https://www.cpird.in.th/slot-gacor-maxwin/">https://www.cpird.in.th/slot-gacor-maxwin/</a>
+<a style="display:none" href="https://anabaptismtoday.co.uk/styles/slot88/">https://anabaptismtoday.co.uk/styles/slot88/</a>
+<a style="display:none" href="https://kiadvany.magyarhonvedseg.hu/public/journals/2/slot-deposit-dana/">https://kiadvany.magyarhonvedseg.hu/public/journals/2/slot-deposit-dana/</a>
+<a style="display:none" href="https://anabaptismtoday.co.uk/styles/slot88/">https://anabaptismtoday.co.uk/styles/slot88/</a>
+<a style="display:none" href="https://penerbit.undip.ac.id/public/site/slot-gacor/">https://penerbit.undip.ac.id/public/site/slot-gacor/</a>
+<a style="display:none" href="https://anabaptismtoday.co.uk/public/site/slot-gacor/">https://anabaptismtoday.co.uk/public/site/slot-gacor/</a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+                            
+            </div>
+          </div>
+        </div>
+      </div>
+    </section> 
+  </div>  
+  <!-- end main-content -->
+
